d836187b038a5bfd5caf288fbdf6d681a77a79b3
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-03-15 00:54:40 +0100

    1.18.2 port

--- src/main/java/fuzs/magnumtorch/MagnumTorch.java
+++ src/main/java/fuzs/magnumtorch/MagnumTorch.java
@@ -16,15 +16,15 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(MagnumTorch.MOD_ID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MagnumTorch {
     public static final String MOD_ID = "magnumtorch";
     public static final String MOD_NAME = "Magnum Torch";
-    public static final Logger LOGGER = LogManager.getLogger(MagnumTorch.MOD_NAME);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     @SuppressWarnings("Convert2MethodRef")
     public static final ConfigHolder<AbstractConfig, ServerConfig> CONFIG = ConfigHolder.server(() -> new ServerConfig());
