e4267884ed8ca033df4878fa30516e27e8de52ef
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-19 16:16:18 +0100

    handle chicken jockeys

--- 1.20.4/Common/src/main/java/fuzs/magnumtorch/MagnumTorch.java
+++ 1.20.4/Common/src/main/java/fuzs/magnumtorch/MagnumTorch.java
@@ -7,6 +7,7 @@ import fuzs.puzzleslib.api.config.v3.ConfigHolder;
 import fuzs.puzzleslib.api.core.v1.ModConstructor;
 import fuzs.puzzleslib.api.core.v1.context.CreativeModeTabContext;
 import fuzs.puzzleslib.api.event.v1.entity.ServerEntityLevelEvents;
+import fuzs.puzzleslib.api.event.v1.server.ServerTickEvents;
 import fuzs.puzzleslib.api.item.v2.CreativeModeTabConfigurator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -27,6 +28,7 @@ public class MagnumTorch implements ModConstructor {
     }
 
     private static void registerHandlers() {
+        ServerTickEvents.END.register(MobSpawningHandler::onEndServerTick);
         ServerEntityLevelEvents.SPAWN.register(MobSpawningHandler::onEntitySpawn);
     }
 
